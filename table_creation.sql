4ee550f5598b61354f2d0454199e57a68400f112
edrossy <edw.rossy@gmail.com>
2018-09-25 22:42:32 -0500

    Update table_creation.sql

--- table_creation.sql
+++ table_creation.sql
@@ -1,107 +1,111 @@
-CREATE TABLE pitches(
-   retro_game_id  VARCHAR(12) NULL
-  ,year           INTEGER  NULL
-  ,st_fl          VARCHAR(1) NULL
-  ,regseason_fl   VARCHAR(1) NULL
-  ,playoffs_fl    VARCHAR(1) NULL
-  ,game_type      VARCHAR(1) NULL
-  ,game_type_des  VARCHAR(256) NULL
-  ,game_id        VARCHAR(11)  NULL
-  ,home_team_id   VARCHAR(11) NULL
-  ,home_team_lg   VARCHAR(11) NULL
-  ,away_team_id   VARCHAR(11) NULL
-  ,away_team_lg   VARCHAR(11) NULL
-  ,interleague_fl VARCHAR(1) NULL
-  ,inning		  INTEGER NULL
-  ,bat_home_id    VARCHAR(1)  NULL
-  ,park_id        INTEGER  NULL
-  ,park_name      VARCHAR(256) NULL
-  ,park_loc       VARCHAR(256) NULL
-  ,pit_id         INTEGER  NULL
-  ,bat_id         INTEGER  NULL
-  ,pit_hand_cd    VARCHAR(1) NULL
-  ,bat_hand_cd    VARCHAR(1) NULL
-  ,pa_ball_ct     INTEGER  NULL
-  ,pa_strike_ct   INTEGER  NULL
-  ,outs_ct        INTEGER  NULL
-  ,pitch_seq      VARCHAR(256) NULL
-  ,pa_terminal_fl VARCHAR(1) NULL
-  ,pa_event_cd    VARCHAR(11)  NULL
-  ,start_bases_cd INTEGER  NULL
-  ,end_bases_cd   INTEGER  NULL
-  ,event_outs_ct  INTEGER  NULL
-  ,ab_number      INTEGER  NULL
-  ,pitch_res      VARCHAR(11) NULL
-  ,pitch_des      VARCHAR(256) NULL
-  ,pitch_id       INTEGER  NULL
-  ,x              VARCHAR(11)  NULL
-  ,y              VARCHAR(11)  NULL
-  ,start_speed    VARCHAR(11)  NULL
-  ,end_speed      VARCHAR(11)  NULL
-  ,sz_top         VARCHAR(11)
-  ,sz_bottom      VARCHAR(11)
-  ,pfx_x          VARCHAR(11)  NULL
-  ,pfx_z          VARCHAR(11)  NULL
-  ,px             VARCHAR(11)  NULL
-  ,pz             VARCHAR(11)  NULL
-  ,x0             VARCHAR(11)  NULL
-  ,y0             VARCHAR(11)  NULL
-  ,z0             VARCHAR(11)  NULL
-  ,vx0            VARCHAR(11)  NULL
-  ,vy0            VARCHAR(11)  NULL
-  ,vz0            VARCHAR(11)  NULL
-  ,ax             VARCHAR(11)  NULL
-  ,ay             VARCHAR(11)  NULL
-  ,az             VARCHAR(11)  NULL
-  ,break_y        VARCHAR(11)  NULL
-  ,break_angle    VARCHAR(11)  NULL
-  ,break_length   VARCHAR(11)  NULL
-  ,pitch_type     VARCHAR(2) NULL
-  ,pitch_type_seq VARCHAR(512) NULL
-  ,type_conf      VARCHAR(256)  NULL
-  ,zone           VARCHAR(11)  NULL
-  ,spin_dir       VARCHAR(11)  NULL
-  ,spin_rate      VARCHAR(11)  NULL
-  ,sv_id          VARCHAR(13) NULL
-);
+CREATE TABLE `pitches` (
+  `index` bigint(20) DEFAULT NULL,
+  `retro_game_id` varchar(12) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `year` int(11) DEFAULT NULL,
+  `st_fl` varchar(1) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `regseason_fl` varchar(1) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `playoffs_fl` varchar(1) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `game_type` varchar(1) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `game_type_des` varchar(256) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `game_id` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `home_team_id` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `home_team_lg` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `away_team_id` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `away_team_lg` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `interleague_fl` varchar(1) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `inning` int(11) DEFAULT NULL,
+  `bat_home_id` varchar(1) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `park_id` int(11) DEFAULT NULL,
+  `park_name` varchar(256) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `park_loc` varchar(256) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `pit_id` int(11) DEFAULT NULL,
+  `bat_id` int(11) DEFAULT NULL,
+  `pit_hand_cd` varchar(1) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `bat_hand_cd` varchar(1) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `pa_ball_ct` int(11) DEFAULT NULL,
+  `pa_strike_ct` int(11) DEFAULT NULL,
+  `outs_ct` int(11) DEFAULT NULL,
+  `pitch_seq` varchar(256) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `pa_terminal_fl` varchar(1) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `pa_event_cd` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `start_bases_cd` int(11) DEFAULT NULL,
+  `end_bases_cd` int(11) DEFAULT NULL,
+  `event_outs_ct` int(11) DEFAULT NULL,
+  `ab_number` int(11) DEFAULT NULL,
+  `pitch_res` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `pitch_des` varchar(256) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `pitch_id` int(11) DEFAULT NULL,
+  `x` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `y` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `start_speed` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `end_speed` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `sz_top` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `sz_bot` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `pfx_x` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `pfx_z` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `px` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `pz` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `x0` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `y0` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `z0` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `vx0` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `vy0` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `vz0` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `ax` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `ay` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `az` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `break_y` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `break_angle` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `break_length` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `pitch_type` varchar(2) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `pitch_type_seq` varchar(512) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `type_conf` varchar(256) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `zone` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `spin_dir` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `spin_rate` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `sv_id` varchar(13) COLLATE utf8_unicode_ci DEFAULT NULL,
+  KEY `index_pitches_index` (`index`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-CREATE TABLE atbats(
-   retro_game_id   VARCHAR(12)
-  ,year            INTEGER
-  ,month           INTEGER
-  ,day             INTEGER
-  ,st_fl           VARCHAR(1)
-  ,regseason_fl    VARCHAR(1)
-  ,playoff_fl      VARCHAR(1)
-  ,game_type       VARCHAR(1)
-  ,game_type_des   VARCHAR(15)
-  ,local_game_time VARCHAR(5)
-  ,game_id         VARCHAR(7)
-  ,home_team_id    VARCHAR(3)
-  ,away_team_id    VARCHAR(3)
-  ,home_team_lg    VARCHAR(2)
-  ,away_team_lg    VARCHAR(2)
-  ,interleague_fl  VARCHAR(1)
-  ,park_id         INTEGER
-  ,park_name       VARCHAR(256)
-  ,park_location   VARCHAR(256)
-  ,inning_number   INTEGER
-  ,bat_home_id     VARCHAR(1)
-  ,inn_outs        INTEGER
-  ,ab_number       INTEGER
-  ,pit_mlbid       INTEGER
-  ,pit_hand_cd     VARCHAR(1)
-  ,bat_mlbid       INTEGER
-  ,bat_hand_cd     VARCHAR(1)
-  ,ball_ct         INTEGER
-  ,strike_ct       INTEGER
-  ,pitch_seq       VARCHAR(64)
-  ,pitch_type_seq  VARCHAR(256)
-  ,event_outs_ct   INTEGER
-  ,ab_des          VARCHAR(256)
-  ,event_tx        VARCHAR(16)
-  ,event_cd        INTEGER
-  ,battedball_cd   VARCHAR(1)
-  ,start_bases_cd  INTEGER
-  ,end_bases_cd    INTEGER
-);
+CREATE TABLE `atbats` (
+  `index` bigint(20) DEFAULT NULL,
+  `retro_game_id` varchar(12) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `year` int(11) DEFAULT NULL,
+  `month` int(11) DEFAULT NULL,
+  `day` int(11) DEFAULT NULL,
+  `st_fl` varchar(1) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `regseason_fl` varchar(1) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `playoff_fl` varchar(1) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `game_type` varchar(1) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `game_type_des` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `local_game_time` varchar(5) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `game_id` varchar(7) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `home_team_id` varchar(3) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `away_team_id` varchar(3) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `home_team_lg` varchar(5) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `away_team_lg` varchar(5) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `interleague_fl` varchar(1) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `park_id` int(11) DEFAULT NULL,
+  `park_name` varchar(256) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `park_location` varchar(256) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `inning_number` int(11) DEFAULT NULL,
+  `bat_home_id` varchar(1) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `inn_outs` int(11) DEFAULT NULL,
+  `ab_number` int(11) DEFAULT NULL,
+  `pit_mlbid` int(11) DEFAULT NULL,
+  `pit_hand_cd` varchar(1) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `bat_mlbid` int(11) DEFAULT NULL,
+  `bat_hand_cd` varchar(1) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `ball_ct` int(11) DEFAULT NULL,
+  `strike_ct` int(11) DEFAULT NULL,
+  `pitch_seq` varchar(512) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `pitch_type_seq` varchar(512) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `event_outs_ct` int(11) DEFAULT NULL,
+  `ab_des` varchar(512) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `event_tx` varchar(128) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `event_cd` int(11) DEFAULT NULL,
+  `battedball_cd` varchar(1) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `start_bases_cd` int(11) DEFAULT NULL,
+  `end_bases_cd` int(11) DEFAULT NULL,
+  KEY `ix_atbats_index` (`index`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
