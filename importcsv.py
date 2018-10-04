489ae65d4dc0589492da4a947a7bb5e030d431bc
edrossy <edw.rossy@gmail.com>
2018-10-03 21:59:29 -0500

    Update importcsv.py

--- importcsv.py
+++ importcsv.py
@@ -6,25 +6,38 @@ import time
 import os
 import unicodecsv as csv
 import zipfile
+import logging.config
 
 def importfile(prefix):
-    constring = 'mysql+mysqldb://sqlusername:sqlpassword@127.0.0.1:3306/pitchfx'
+    logfile = (prefix + '_importlog_').upper() + str(time.strftime('%Y%m%d')) + '.log'
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s.%(msecs)03d|%(levelname)s|{%(module)s}%(message)s',
+                        datefmt='%Y-%m-%d,%H:%M:%S',
+                        filename=logfile,
+                        filemode='a')
+    # define a Handler which writes INFO messages or higher to the sys.stderr
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    # set a format which is simpler for console use
+    formatter = logging.Formatter('%(name)-12s|%(levelname)-8s %(message)s')
+    # tell the handler to use this format
+    console.setFormatter(formatter)
+    # add the handler to the root logger
+    logging.getLogger('').addHandler(console)
+    constring = 'mysql+mysqldb://username:password@127.0.0.1:3306/pitchfx'
     chunksize = 50000
     delimiter = ","
     textqualifier = '"'
     tfolder = "import/"
     startscript = time.time()
-
     xx = 1
     for root, dirs, files in os.walk(tfolder):
         for file in files:
             if file.startswith(prefix):
                 if file.endswith('.csv'):
                     tfile = (tfolder + file)
-
                     #Set MySQL Table Name - Note Tables (pitchfx.pitches & pitchfx.atbats) in MySQL DB must already be created - see SQL Scripts
                     mytable = file.replace('.csv', '').replace(prefix,'')
-
                     #Open CSV File
                     f = open(tfile, 'rb')
                     engine = create_engine(constring)
@@ -35,8 +48,7 @@ def importfile(prefix):
                     start = dt.datetime.now()
                     j = 0
                     index_start = 1
-                    print ((str(datetime.datetime.now()) + ' ;Importing File: ' + tfile))
-
+                    logging.info((str(datetime.datetime.now()) + ' ;Importing File: ' + tfile))
                     #Get rows from CSV file and being import into MySQL DB
                     for df in pd.read_csv(f, chunksize=chunksize, iterator=True, encoding='utf-8',
                                           delimiter=str(delimiter), quotechar=str(textqualifier),
@@ -45,34 +57,30 @@ def importfile(prefix):
                         #This set index column value for MySQL import
                         df.index += index_start
                         j += 1 #Row Count
-                        print (str(datetime.datetime.now()) + ' ;{} seconds: completed {} rows'.format((dt.datetime.now() - start).seconds, j * chunksize))
                         df.to_sql(mytable, engine, if_exists='append')
+                        logging.info(str(datetime.datetime.now()) + ' ;{} seconds: completed {} rows'.format((dt.datetime.now() - start).seconds, j * chunksize))
                         index_start = df.index[-1] + 1
                     f.close()
                     xx += 1
-                    print ((str(datetime.datetime.now()) + ' ;Completed Importing File to Database: ' + tfile))
-
+                    logging.info(str(datetime.datetime.now()) + ' ;Completed Importing File to Database: ' + tfile)
                     #Zip File For Archiving Purposes And To Avoind Further Processing
                     try:
                         import zlib
                         compression = zipfile.ZIP_DEFLATED
                     except:
                         compression = zipfile.ZIP_STORED
-
                     modes = {zipfile.ZIP_DEFLATED: 'deflated', zipfile.ZIP_STORED: 'stored', }
-                    print ((str(datetime.datetime.now()) + ' ;Archiving File: ' + tfile))
+                    logging.info(str(datetime.datetime.now()) + ' ;Archiving File: ' + tfile)
                     zf = zipfile.ZipFile(tfile.replace('.csv','') + '.zip', mode='w')
                     try:
-                        print ((str(datetime.datetime.now()) + ' ;Archiving File: ' + tfile + ' with compression mode',
-                                modes[compression]))
+                        logging.info(str(datetime.datetime.now()) + ' ;Archiving File: ' + tfile + ' with compression mode',
+                                modes[compression])
                         zf.write(tfile, compress_type=compression)
                     finally:
                         zf.close()
-
                     # Delete the imported CSV file
-                    print ((str(datetime.datetime.now()) + ' ;Deleting File: ' + tfile))
+                    logging.info(str(datetime.datetime.now()) + ' ;Deleting File: ' + tfile)
                     os.remove(tfile)
-
-    print (str(datetime.datetime.now()) + ' ;End of job')
+    logging.info(str(datetime.datetime.now()) + ' ;End of job')
     endscript = time.time()
-    print (str(datetime.datetime.now()) + ' ;Elapsed time: '+'{:10.2f}'.format(endscript - startscript) + ' seconds or ' + '{:10.2f}'.format((endscript - startscript) / 60) + ' minutes')
+    logging.info(str(datetime.datetime.now()) + ' ;Elapsed time: '+'{:10.2f}'.format(endscript - startscript) + ' seconds or ' + '{:10.2f}'.format((endscript - startscript) / 60) + ' minutes')
